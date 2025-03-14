f0db9299ea1d903da0f28ec9027a8cc854d444ae
addefj <adde.fj@gmail.com>
2025-03-14 14:00:01 +0100

    updated comments

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -3,15 +3,23 @@ package se.lexicon;
 public class Main {
     public static void main(String[] args) {
        /*
-step 1. create class with method for printing menu
-step 2. take care of user input
-step 3. validate user input
-step 4. create class with method for currency conversion
-step 5. create method for displaying result formatted
+step 1. create class with methods for displaying menus, and exchange results
+    1.1 main menu
+    1.2 exchange menu
+    1.3 error messages
+    1.4 Exit message
+    1.5 formatted result
+step 2. create class exchange rates
+step 3. create class with method for currency conversion
+    3.1 switch for performing different menu options
+step 4. create class with methods for inputs and validations
+    4.1 input, read and validate main menu input
+    4.2 input, read and validate amount input
+step 5. Optional improvements
+    5.1 option to return to main menu
  */
 
         int userChoice = UserInputValidation.isMenuInputValid(); //ask user for menu choice and validate the input
         ExchangeOperation.exchangeCurrency(userChoice); //call method for exchanging currency
-
     }
 }
